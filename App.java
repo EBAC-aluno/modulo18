66c59b94efcbeec2cd60778a67598a1bad302106
Pedro Lobo <lobophf@gmail.com>
2022-10-23 07:41:36 -0300

    Update App.java

--- App.java
+++ App.java
@@ -1,12 +1,10 @@
-class Table{
+@interface Tabela{
+  public String name();
 
 }
 
-public class App{
-  public static void main(String args[]){
-      
-   @Tabela(name = "TABLE_NAME")
-   Table t = new Table(); 
+public class App {
+  public static void main(String args[]) {
 
   }
 }
